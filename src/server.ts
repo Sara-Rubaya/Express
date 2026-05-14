4ccab862bc6b8f98e389de25ee4546bb82bd3121
Sara-Rubaya <sararubaya4800@gmail.com>
2026-05-14 13:53:10 +0600

    executed pool & created tables

--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,30 @@ app.use(express.text());
 app.use(express.urlencoded({extended : true}));
 
 const pool = new Pool({
-    connectionString : "postgresql://neondb_owner:npg_k1eSNmQUzW4C@ep-dawn-rice-apw0prz6.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require",
+    connectionString : "postgresql://neondb_owner:npg_k1eSNmQUzW4C@ep-dawn-rice-apw0prz6-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 });
 
+const initDB = async()=>{
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(50),
+      email VARCHAR(100) NOT NULL,
+      password VARCHAR(100) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      
+      )
+      `)
+      console.log("Database connected successfully!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+initDB();
 
 app.get("/", (req : Request, res : Response) => {
 //   res.send('Hello World!')
