3740d48b85937612a74106cf25bb69dc2349a209
Sara-Rubaya <sararubaya4800@gmail.com>
2026-05-15 19:33:59 +0600

    updated user with PUT method

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { type Application, type Request, type Response } from "express";
-import {Pool} from "pg";
+import { AsyncLocalStorage } from "node:async_hooks";
+import {Pool, Result} from "pg";
 const app : Application = express();
 const port = 5000;
 
@@ -95,7 +96,7 @@ app.get("/api/users/:id",async(req : Request, res : Response)=>{
         `,[id],);
 
         if(result.rows.length ===0){
-           res.status(500).json({
+           res.status(404).json({
         success :  false,
         messagse : "User not found!",
         data :  {},
@@ -114,8 +115,46 @@ app.get("/api/users/:id",async(req : Request, res : Response)=>{
         data :  error,
     });
     }
-})
+});
+
+app.put("/api/users/:id", async(req :  Request, res : Response)=>{
+  const {id} = req.params;
+  const {name, password,age,is_active} = req.body;
+  // console.log("Id : " ,id);
+  // console.log({name, password,age,is_active});
+
+  try {
+    const result = await pool.query(`
+    UPDATE users SET name = $1,
+    password=$2,
+    age=$3,
+    is_active=$4 WHERE id=$5 RETURNING *
+    `,[name,password,age,is_active,id],
+  );
+  // console.log(result)
+
+  if(result.rows.length===0){
+    res.status(404).json({
+      success : false,
+      message : "User not found!",
+      // data: {},
+    });
+  }
+
+  res.status(200).json({
+        success :  true,
+        messagse : "User Updated successfully!",
+        data :  result.rows[0],
+  });
+  } catch (error : any) {
+     res.status(500).json({
+        success :  false,
+        messagse : error.message,
+        data :  error,
+    });
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
