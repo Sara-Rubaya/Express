8009d1d0d42a548ea7472108a7589c2e66e4f350
Sara-Rubaya <sararubaya4800@gmail.com>
2026-05-20 12:30:00 +0600

    modified server

---
+++ src/db/index.ts
@@ -0,0 +1,32 @@
+import { Pool } from "pg";
+import config from "../config";
+
+export const pool = new Pool({
+    connectionString : config.connection_string,
+});
+
+
+
+
+// Create TABLE in DB
+
+export const initDB = async()=>{
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(50),
+      email VARCHAR(100) UNIQUE NOT NULL,
+      password VARCHAR(100) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      
+      )
+      `)
+      console.log("Database connected successfully!");
+  } catch (error) {
+    console.log(error);
+  }
+};
