cd87e15e4944245f93285b904db136dadd42a2a2
Sara-Rubaya <sararubaya4800@gmail.com>
2026-05-21 13:29:05 +0600

    express midleware

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import { initDB, pool } from "./db";
 import { userRoute } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { authRoute } from "./modules/auth/auth.route";
+import fs from "fs";
 
 const app : Application = express();
 
@@ -20,9 +21,16 @@ app.use(express.text());
 app.use(express.urlencoded({extended : true}));
 
 
+app.use((req,resizeBy,next)=>{
+  console.log("Method - URL - Time :",req.method, req.url, Date.now());
+  const log = `\nMethod -> ${req.method} - Time -> ${Date.now} - URL -> ${req.url}\n`; 
+  fs.appendFile("logger.txt", log, (err) =>{
+    console.log(err);
+    })
+  next();
+});
+
 // Connect to NEONDB
-
-
 initDB();
 
 app.get("/", (req : Request, res : Response) => {
