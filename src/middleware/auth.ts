d6ec7c9b466b59e7504ec37ade139322ce3dc380
Sara-Rubaya <sararubaya4800@gmail.com>
2026-05-21 20:07:16 +0600

    auth middleware validation

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,11 +1,15 @@
 import type { NextFunction, Request, Response } from "express";
-import { Result } from "pg";
+import jwt, { type JwtPayload } from "jsonwebtoken"
+import config from "../config";
+import { pool } from "../db";
 
 const auth = ()=>{
     return async (req : Request, res : Response, next : NextFunction)=>{
     // console.log("this is protected route");
     // console.log(req.headers.authorization);
     const token = req.headers.authorization;
+
+    // console.log(token);
 
     if(!token){
         res.status(401).json({
@@ -14,6 +18,33 @@ const auth = ()=>{
             
         });
     }
+
+    const decoded = jwt.verify(token as string,config.secret as string) as JwtPayload;
+    
+    const userData = await pool.query(`
+       SELECT * FROM users WHERE email=$1 
+        `,[decoded.email],
+    );
+    // console.log(userData);
+    const user = userData.rows[0];
+    // console.log(user);
+    if(userData.rows.length === 0){
+      res.status(404).json({
+            success : false,
+            message : "user not found!!!",
+            
+        });
+    }
+
+
+    if(!user.is_active){
+       res.status(403).json({
+            success : false,
+            message : "Forbidden!!!",
+            
+        });
+    }
+
     next();
 
 };
