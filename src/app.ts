92bea4e542f0a7915a868957e5232ea2de63ff5a
Sara-Rubaya <sararubaya4800@gmail.com>
2026-05-21 23:56:14 +0600

    Global Error Handling Architecture

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,8 @@ import { profileRoute } from "./modules/profile/profile.route";
 import { authRoute } from "./modules/auth/auth.route";
 import logger from "./middleware/logger";
 import CookieParser from "cookie-parser";
+import cors from "cors";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 
 const app : Application = express();
 
@@ -21,6 +23,11 @@ app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({extended : true}));
 app.use(logger);
+app.use(
+  cors({
+   origin: 'http://localhost:5000',
+}),
+);
 
 // Connect to NEONDB
 initDB();
@@ -37,4 +44,7 @@ app.use("/api/users",userRoute);
 app.use("/api/profile",profileRoute);
 app.use("/api/auth",authRoute)
 
+
+// Global Error Handling Middleware
+app.use(globalErrorHandler);
 export default app
