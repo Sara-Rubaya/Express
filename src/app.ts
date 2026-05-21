e88ca1f07bd62c1f49fd340d7d05505c8797bb13
Sara-Rubaya <sararubaya4800@gmail.com>
2026-05-21 11:09:10 +0600

    added hashed password

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import express, {
 import config from "./config";
 import { initDB, pool } from "./db";
 import { userRoute } from "./modules/user/user.route";
+import { profileRoute } from "./modules/profile/profile.route";
 
 const app : Application = express();
 
@@ -33,6 +34,6 @@ app.get("/", (req : Request, res : Response) => {
 
 app.use("/api/users",userRoute);
 
-
+app.use("/api/profile",profileRoute);
 
 export default app
