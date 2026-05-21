a067932e1594fa6e097963a53ae7d2dcd188ad44
Sara-Rubaya <sararubaya4800@gmail.com>
2026-05-21 14:15:39 +0600

    created auth middleware

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import { userRoute } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { authRoute } from "./modules/auth/auth.route";
 import fs from "fs";
+import logger from "./middleware/logger";
 
 const app : Application = express();
 
@@ -19,16 +20,7 @@ const app : Application = express();
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({extended : true}));
-
-
-app.use((req,resizeBy,next)=>{
-  console.log("Method - URL - Time :",req.method, req.url, Date.now());
-  const log = `\nMethod -> ${req.method} - Time -> ${Date.now} - URL -> ${req.url}\n`; 
-  fs.appendFile("logger.txt", log, (err) =>{
-    console.log(err);
-    })
-  next();
-});
+app.use(logger);
 
 // Connect to NEONDB
 initDB();
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,27 +1,25 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
+import auth from "../../middleware/auth";
 
 
 const router = Router()
 
-// CREATE user
 
+
+// CREATE user
 router.post("/",userController.createUser);
 
 // GEt all users
-
-router.get("/",userController.getAllUsers );
+router.get("/", auth(), userController.getAllUsers );
 
 // GET single user/by id
-
 router.get("/:id",userController.getSingleUser);
 
 // UPDATE user by id
-
 router.put("/:id",userController.updateUser );
 
 // DELETE user by id
-
 router.delete("/:id",userController.deleteUser );
 
 
